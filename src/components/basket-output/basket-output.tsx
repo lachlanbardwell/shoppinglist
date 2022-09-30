b7ddd829006ff9164b0a7b5f50a73956e2de4a1c
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2022-09-30 22:18:46 +1000

    css

--- src/components/basket-output/basket-output.tsx
+++ src/components/basket-output/basket-output.tsx
@@ -4,6 +4,7 @@ import { itemCostTotal } from '../../transformers/item-cost';
 import { IBasketOutput } from '../../types';
 import { Avatar, Paper, Tooltip } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
+import RemoveIcon from '@material-ui/icons/Remove';
 import './basket-output.css';
 
 export const BasketOutput: React.FC<IBasketOutput> = (props) => {
@@ -20,52 +21,63 @@ export const BasketOutput: React.FC<IBasketOutput> = (props) => {
                   className="basket-item-image"
                   src={`https://logo.clearbit.com/${next.store}.com.au`}
                 />
-                <p>{next.id}</p>
+                <p className="basket-item-id">{next.id}</p>
                 <p className="basket-item-price">{`$${next.price.toFixed(2)} ${
                   next.perkg ? 'per kg' : 'ea'
                 }`}</p>
               </span>
-              <span className="basket-quantity">
-                <Avatar
-                  className="basket-minus"
-                  onClick={() => props.changeQuantity(next, 'subtract')}
+              <div className="basket-quantity">
+                <span className="basket-numbers">
+                  <Avatar
+                    className="basket-minus"
+                    onClick={() => props.changeQuantity(next, 'subtract')}
+                    style={{
+                      backgroundColor: 'black',
+                      margin: 'auto',
+                      width: 18,
+                      height: 18,
+                    }}
+                  >
+                    -
+                  </Avatar>
+                  &nbsp;
+                  <Avatar
+                    className="basket-quantity-count"
+                    style={{
+                      backgroundColor: 'white',
+                      border: '1px solid black',
+                      color: 'black',
+                      width: 26,
+                      height: 26,
+                    }}
+                  >
+                    {next.quantity}
+                  </Avatar>
+                  &nbsp;
+                  <Avatar
+                    className="basket-plus"
+                    onClick={() => props.changeQuantity(next, 'add')}
+                    style={{
+                      backgroundColor: 'black',
+                      margin: 'auto',
+                      width: 18,
+                      height: 18,
+                    }}
+                  >
+                    +
+                  </Avatar>
+                </span>
+                <RemoveIcon
+                  preserveAspectRatio="none"
+                  color="action"
                   style={{
-                    backgroundColor: 'black',
                     margin: 'auto',
-                    width: 18,
-                    height: 18,
+                    height: '10px',
+                    width: '100%',
                   }}
-                >
-                  -
-                </Avatar>
-                &nbsp;
-                <Avatar
-                  className="basket-quantity-count"
-                  style={{
-                    backgroundColor: 'white',
-                    border: '1px solid black',
-                    color: 'black',
-                    width: 26,
-                    height: 26,
-                  }}
-                >
-                  {next.quantity}
-                </Avatar>
-                &nbsp;
-                <Avatar
-                  className="basket-plus"
-                  onClick={() => props.changeQuantity(next, 'add')}
-                  style={{
-                    backgroundColor: 'black',
-                    margin: 'auto',
-                    width: 18,
-                    height: 18,
-                  }}
-                >
-                  +
-                </Avatar>
+                />
                 <p className="item-total">{itemCostTotal(next)}</p>
-              </span>
+              </div>
               <Tooltip title="Remove item">
                 <span
                   className="remove-single-item"
